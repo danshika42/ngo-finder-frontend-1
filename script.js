3cd60d71abe4e7823ad28cfc240fcbf0940a3930
danshika42 <83402199+danshika42@users.noreply.github.com>
2021-11-02 12:12:38 +0530

    --updated nav toggle

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var rightMenu=document.querySelector('.right-menu');
 var menuBtn=document.querySelector('.menu-btn');
 var showcaseContent=document.querySelector('.showcase-content');
 var heading=document.querySelector('.nav-bar a');
-
+var home=document.getElementById('home');
 menuBtn.addEventListener('click', () => {
     mainMenu.classList.toggle('show')
     rightMenu.classList.toggle('showRightmenu')
     showcaseContent.classList.toggle('hide')
     heading.classList.toggle('hide2')
 });
+home.addEventListener('click',()=>{
+    mainMenu.classList.toggle('show')
+    rightMenu.classList.toggle('showRightmenu')
+    showcaseContent.classList.toggle('hide')
+    heading.classList.toggle('hide2')
+})
 
